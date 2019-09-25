59bf81d7df771d92d5f579414a1a09463d701268
LUIS J Alvarez Diaz <ldiaz244@gmail.com>
2019-09-25 20:15:58 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,12 @@
 class Song
+  Song.find_or_create_by_name("Blank Space")
+  song = Song.create
+  Song.all.include?(song)
   attr_accessor :name, :artist_name
-  @@all = []
+  @@all = ["The Middle"]
 
   def self.all
-    @@all
+    @@all = name("The Middle")
   end
 
   def save
